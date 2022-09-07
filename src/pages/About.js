3cd73108d076ddfad1fe7193b00c1a25b47a73b5
sgulamali76 <sguacamole98@gmail.com>
2022-09-07 17:22:54 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint alias
+        omnis maiores iusto, nesciunt ut doloribus modi quia quod magnam
+        dignissimos porro eos odit vitae, suscipit, consequuntur accusamus saepe
+        voluptatem!
+      </p>
+    </section>
+  );
+};
+
+export default About;
